8aa8544f1c13b5eeda73416a79003c327a3552a9
DILANTHA9590 <oyage-email@example.com>
2025-08-26 22:54:31 +0530

    setup sql db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,39 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { UserModule } from './api/user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    
+    
+    // ConfigModule.forRoot({
+    //   isGlobal: true,
+    //   envFilePath: '.env',
+    // }),
+    // TypeOrmModule.forRootAsync({
+    //   imports: [ConfigModule],
+    //   useFactory: async (configService: ConfigService) => ({
+    //     type: 'mysql',
+    //     host: configService.get<string>('DB_HOST'),
+    //     port: configService.get<number>('DB_PORT'),
+    //     username: configService.get<string>('DB_USERNAME'),
+    //     password: configService.get<string>('DB_PASS'),
+    //     database: configService.get<string>('DB_NAME'),
+    //     entities: [],
+    //     autoLoadEntities: true,
+    //     synchronize: true,
+    //     logging: true,
+    //   }),
+    //   inject: [ConfigService],
+    // }),
+    // TypeOrmModule.forFeature([
+    
+    // ]),
+  
+  
+  ],
+
 })
 export class AppModule {}
