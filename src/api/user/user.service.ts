13f179efb43d2b4c28ef1baa70fa7eec6859d35e
DILANTHA9590 <oyage-email@example.com>
2025-08-27 22:53:08 +0530

    create user creation function

--- src/api/user/user.service.ts
+++ src/api/user/user.service.ts
@@ -1,17 +1,51 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import {  Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import argon2 from 'argon2';
+
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+
+  ) {}
+  async create(createUserDto: CreateUserDto) {
+
+    
+    const {email ,password} = createUserDto;
+
+
+    const existingUser = await this.userRepository.findOne({where:{email:email}});
+    if(existingUser){
+
+      throw new Error('User with this email already exists');
+
+    }
+
+    const hashPassword = await argon2.hash(password);
+
+    const newUser = this.userRepository.save({
+      ...createUserDto,
+      password:hashPassword
+    });
+
+
+    return{
+      message : 'User created successfully',
+      user : newUser
+    }
+
   }
 
-  findAll() {
-    return `This action returns all user`;
-  }
 
+
+  
   findOne(id: number) {
     return `This action returns a #${id} user`;
   }
