62e26fe8119b61d49384f346beab8c919caa592e
DILANTHA9590 <oyage-email@example.com>
2025-08-27 22:52:20 +0530

    set database connectin🌟

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,35 +3,36 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './api/user/user.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './api/user/entities/user.entity';
 
 @Module({
   imports: [
     
     
-    // ConfigModule.forRoot({
-    //   isGlobal: true,
-    //   envFilePath: '.env',
-    // }),
-    // TypeOrmModule.forRootAsync({
-    //   imports: [ConfigModule],
-    //   useFactory: async (configService: ConfigService) => ({
-    //     type: 'mysql',
-    //     host: configService.get<string>('DB_HOST'),
-    //     port: configService.get<number>('DB_PORT'),
-    //     username: configService.get<string>('DB_USERNAME'),
-    //     password: configService.get<string>('DB_PASS'),
-    //     database: configService.get<string>('DB_NAME'),
-    //     entities: [],
-    //     autoLoadEntities: true,
-    //     synchronize: true,
-    //     logging: true,
-    //   }),
-    //   inject: [ConfigService],
-    // }),
-    // TypeOrmModule.forFeature([
-    
-    // ]),
-  
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASS'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [User],
+        autoLoadEntities: true,
+        synchronize: true,
+        logging: true,
+      }),
+      inject: [ConfigService],
+    }),
+    TypeOrmModule.forFeature([
+      User]),
+      
+  UserModule
   
   ],
 
