8da27482c9027823a0a39dc34dc6a88faf69764d
DILANTHA9590 <oyage-email@example.com>
2025-08-29 23:12:26 +0530

    CREAT LOGIN AUTH CONTROLLER

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -10,14 +10,21 @@ import {
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { CreateUserDto } from '../user/dto/create-user.dto';
+import { LoginUserDto } from '../user/dto/user-login.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post()
-  create(@Body() createAuthDto: CreateAuthDto) {
+  create(@Body() createAuthDto: CreateUserDto) {
     return this.authService.create(createAuthDto);
+  }
+
+ @Post('login')
+  loginUser(@Body() loginAuthDto: LoginUserDto) {
+    return this.authService.loginUser(loginAuthDto);
   }
 
   @Get()
