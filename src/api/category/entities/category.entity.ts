5299d517fa7d31cec5bc1670d7c82cc89afc94a7
DILANTHA9590 <oyage-email@example.com>
2025-08-31 10:34:21 +0530

    Created Category entity with id, image, categoryName, description, and OneToMany relationship with Menuitem 🌿✨

---
+++ src/api/category/entities/category.entity.ts
@@ -0,0 +1,22 @@
+import { Menuitem } from "src/api/menuitem/entities/menuitem.entity";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("categories")
+export class Category {
+
+    @PrimaryGeneratedColumn("uuid")
+    id:string
+
+    @Column("simple-json", {nullable: true})
+    image:string
+
+    @Column({nullable: true , unique: true})
+    categoryName:string
+
+    @Column("text",{nullable: true})
+    description:string
+
+
+@OneToMany(() => Menuitem, (menuitem) => menuitem.category)
+menuitems: Menuitem[]
+}
