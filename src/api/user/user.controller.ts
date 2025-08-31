dbe15d9ece45f61c0bf214d3f388ea1a105384d3
DILANTHA9590 <oyage-email@example.com>
2025-08-31 22:56:25 +0530

    com[leted   getuserbyid, updateuser and deleteuser methods

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -1,3 +1,4 @@
+
 import {
   Controller,
   Get,
@@ -8,6 +9,8 @@ import {
   Delete,
   UseGuards,
   Req,
+  Query,
+  Put,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -16,17 +19,18 @@ import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
 import { RolesGuard } from '../auth/role-guard/roles.guard';
 import { Roles } from '../auth/role-guard/roles.decorator';
 import { Admin } from 'typeorm';
+import { JwtPayload } from '../common/interfaces/jwt-payload.interface';
+import { JwtPayloadDto } from '../common/interfaces/jwt-payload.dto';
 
 @Controller('user')
 export class UserController {
-
   constructor(private readonly userService: UserService) {}
   @UseGuards(JwtAuthGuard ,RolesGuard)
 
 
    @Get('profile')
      @Roles('customer', 'admin')
-  getProfile(@Req() req) {
+async  getProfile(@Req() req) {
     return req.user; // decoded JWT payload
     //  getProfile(@Req() req) {
     // return this.userService.getProfile(req.user); // ✅ user එ
@@ -36,22 +40,37 @@ export class UserController {
   
 
   @Get()
-  findAll() {
-    return this.userService.findAll();
+  @Roles('admin')
+  findAll(@Query('searchTerm') searchTerm: string) {
+    return this.userService.findAll(searchTerm);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
+  @Get("getbyid")
+  @Roles('admin','customer')
+  findOne( @Req()req:{user:JwtPayloadDto} ,id: string) {
+    return this.userService.findOne(id,req.user );
+  }
+  @Put(':id')
+  @Roles('admin','customer')
+  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+const result =await  this.userService.update(id, updateUserDto);
+
+return {
+  message : "User updated successfully",
+  data:result
+}
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
-  }
+
+
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+  @Roles('admin')
+  async remove(@Param('id') id: string) {
+   const result =await this.userService.remove(id);
+
+   if(result){
+  return {message: "User deleted successfully"}
+   }
   }
 }
