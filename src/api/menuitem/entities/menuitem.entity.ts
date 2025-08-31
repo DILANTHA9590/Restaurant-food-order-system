22150d94dc0851ea1c49631f9e43258ae39b4e63
DILANTHA9590 <oyage-email@example.com>
2025-08-31 10:33:57 +0530

    Defined Menuitem entity with fields for id, image, name, altNames, price, lastPrice, description, availability, timestamps, and ManyToOne relationship with Category 🍽️✨

---
+++ src/api/menuitem/entities/menuitem.entity.ts
@@ -0,0 +1,46 @@
+import { Category } from "src/api/category/entities/category.entity";
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToMany, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity("menuitem")
+export class Menuitem {
+
+
+    @PrimaryGeneratedColumn("uuid")
+    id : string
+
+    @Column({nullable:false})
+    image:string
+
+    @Column({nullable:false})
+     name : string
+
+    @Column("json", { nullable:true})
+     altNames : string[]
+
+    @Column({nullable:false, default: 0})
+    price:number
+    
+    @Column({nullable:false, default: 0})
+    lastPrice:number
+
+    @Column("text",{nullable: true})
+    description:string
+
+    @Column({nullable:false ,default: true})
+        availability:boolean
+
+
+    @CreateDateColumn({nullable:false,type:"timestamp"})
+        createdAt: Date;
+
+
+    @UpdateDateColumn({nullable:false,type:"timestamp"})
+        updatedAt: Date;
+
+
+@ManyToOne(() => Category, (category) => category.menuitems)
+@JoinColumn({ name: "cat_id" })
+category: Category;
+
+}
+   
