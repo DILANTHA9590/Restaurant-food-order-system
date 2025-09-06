bca6ee0318b8fcb7be5cc6ad781df53dadad9519
DILANTHA9590 <oyage-email@example.com>
2025-09-06 10:56:31 +0530

    implement create ctegory method✨

--- src/api/category/category.service.ts
+++ src/api/category/category.service.ts
@@ -1,34 +1,70 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Category } from './entities/category.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CategoryService {
-  create(createCategoryDto: CreateCategoryDto) {
-
-    
-
-
-
+  constructor(
+    @InjectRepository(Category)private readonly categoryRepository: Repository<Category>,
+  ){}
+  async create(createCategoryDto: CreateCategoryDto) {
 
 
+    const {categoryName} = createCategoryDto
+
+
+    const existingCategory = await this.categoryRepository.findOne({where:{categoryName:categoryName}})
+
+    if(existingCategory){
+      throw new ConflictException("Category with this name already exists");
+    }
+
+    const category =  this.categoryRepository.create(
+    createCategoryDto 
+    )
+
+  const savedCategory = await this.categoryRepository.save(category);
+
+
+  return{
+    savedCategory ,
+    message:"Category created successfully"
+
+  }
+
+  }
+
+
+
+  async getAllCategory():Promise<{message?:string , categories:Category[]}>{
+
+
+
+    const categories = await this.categoryRepository.find()
+
+
+    if(categories.length==0){
+
+      return{
+        message:"No Category items found",
+        categories
+      }
+    }
+    return {
+      categories
+
+    }
 
 
   }
 
-  findAll() {
-    return `This action returns all category`;
-  }
 
-  findOne(id: number) {
-    return `This action returns a #${id} category`;
-  }
 
-  update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return `This action updates a #${id} category`;
-  }
 
-  remove(id: number) {
-    return `This action removes a #${id} category`;
-  }
+
+
+
 }
