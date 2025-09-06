edfd7e100f59b65394391477b1b927ccab457bb6
DILANTHA9590 <oyage-email@example.com>
2025-09-06 09:16:45 +0530

    add Swagger ApiTags for AuthController

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -12,7 +12,9 @@ import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { CreateUserDto } from '../user/dto/create-user.dto';
 import { LoginUserDto } from '../user/dto/user-login.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags("Auth")
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
