1dce611bc028ed0b64f2eb5f63d7a470d6cbbaa9
DILANTHA9590 <oyage-email@example.com>
2025-09-06 11:37:10 +0530

    protect create category endpoint with JWT auth guard

--- src/api/category/category.controller.ts
+++ src/api/category/category.controller.ts
@@ -1,36 +1,39 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Req, UseGuards, Put } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
 import { JwtPayload } from '../common/interfaces/jwt-payload.interface';
+import { RolesGuard } from '../auth/role-guard/roles.guard';
+import { Roles } from '../auth/role-guard/roles.decorator';
 
 @Controller('category')
+@UseGuards(JwtAuthGuard,RolesGuard)
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
-@UseGuards(JwtAuthGuard)
+
   @Post()
+  @Roles('admin','staff')
   create(@Req() req: {user: JwtPayload}, @Body() createCategoryDto: CreateCategoryDto) {
     return this.categoryService.create(createCategoryDto);
   }
 
   @Get()
   findAll() {
-    return this.categoryService.findAll();
+    return this.categoryService.getAllCategory();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.categoryService.findOne(+id);
-  }
 
-  @Patch(':id')
+
+  @Put(':id')
+  @Roles('admin','staff')
   update(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
-    return this.categoryService.update(+id, updateCategoryDto);
+    return this.categoryService.updateCategory(id, updateCategoryDto);
   }
 
   @Delete(':id')
+  @Roles('admin','staff')
   remove(@Param('id') id: string) {
-    return this.categoryService.remove(+id);
+    return this.categoryService.deleteCategory(id);
   }
 }
