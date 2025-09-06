4f0168bd5149aff1fbf1c1f9b1e6074761b90a97
DILANTHA9590 <oyage-email@example.com>
2025-09-06 15:17:00 +0530

    add CRUD endpoints for menu items with category support

--- src/api/menuitem/menuitem.controller.ts
+++ src/api/menuitem/menuitem.controller.ts
@@ -1,37 +1,34 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Put } from '@nestjs/common';
 import { MenuitemService } from './menuitem.service';
 
 
 
 import { UpdateMenuitemDto } from './dto/update-menuitem.dto';
 import { CreateMenuitemDto } from './dto/create-menuitem.dto';
+import { Category } from '../category/entities/category.entity';
 
 @Controller('menuitem')
 export class MenuitemController {
   constructor(private readonly menuitemService: MenuitemService) {}
 
-  @Post()
-  create(@Body() createMenuitemDto: CreateMenuitemDto) {
-    return this.menuitemService.create(createMenuitemDto);
+  @Post(':categoryId')
+  create( @Param('categoryId') categoryId:string ,   @Body() createMenuitemDto: CreateMenuitemDto) {
+    return this.menuitemService.createMenuItem(categoryId,createMenuitemDto);
   }
 
-  @Get()
-  findAll() {
-    return this.menuitemService.findAll();
+  @Get(':categoryId')
+  findAll(@Param('categoryId') categoryId:string) {
+    return this.menuitemService.findAllCategoryItem(categoryId);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.menuitemService.findOne(+id);
-  }
-
-  @Patch(':id')
+  
+  @Put(':id')
   update(@Param('id') id: string, @Body() updateMenuitemDto: UpdateMenuitemDto) {
-    return this.menuitemService.update(+id, updateMenuitemDto);
+    return this.menuitemService.updateMenuItem(id, updateMenuitemDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.menuitemService.remove(+id);
+    return this.menuitemService.removeMenuItem(id);
   }
 }
