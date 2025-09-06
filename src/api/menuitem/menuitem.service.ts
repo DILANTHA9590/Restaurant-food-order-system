28a1b2527ebbf3b3e8eeaa42835bb21241fb63ef
DILANTHA9590 <oyage-email@example.com>
2025-09-06 15:06:38 +0530

    add create and findAllCategoryItem methods with category validation

--- src/api/menuitem/menuitem.service.ts
+++ src/api/menuitem/menuitem.service.ts
@@ -1,26 +1,99 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateMenuitemDto } from './dto/create-menuitem.dto';
 import { UpdateMenuitemDto } from './dto/update-menuitem.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Menuitem } from './entities/menuitem.entity';
+import { Repository } from 'typeorm';
+import { Category } from '../category/entities/category.entity';
+import { UpdateCategoryDto } from '../category/dto/update-category.dto';
 
 @Injectable()
 export class MenuitemService {
-  create(createMenuitemDto: CreateMenuitemDto) {
-    return 'This action adds a new menuitem';
+
+  constructor(
+    @InjectRepository(Menuitem) private readonly menuItemRepository:Repository<Menuitem>,
+      @InjectRepository(Category)private readonly categoryRepository: Repository<Category>
+  ){}
+async  createMenuItem(categoryId:string, createMenuitemDto: CreateMenuitemDto) {
+
+
+    const {name} = createMenuitemDto
+
+
+    const existingMenuItem =  await this.menuItemRepository.findOne({where:{name}})
+
+    if(existingMenuItem){
+
+      throw new ConflictException("This item name alread have")
+    }
+
+    const category = await this.categoryRepository.findOne({ where: { id: categoryId } });
+
+    if (!category) {
+      throw new NotFoundException(`Category with ID ${categoryId} not found`);
+    }
+
+
+    const newItem = this.menuItemRepository.create({...createMenuitemDto ,
+      category:{id:category.id} as Category
+    })
+
+
+    await this.menuItemRepository.save(newItem);
+
+
+
+    return {
+
+      message : "New item created successfully"
+
+    }
+
+
+
+  
   }
 
-  findAll() {
-    return `This action returns all menuitem`;
+async findAllCategoryItem(categoryId: string) {
+
+  const category = await this.categoryRepository.findOne({ where: { id: categoryId } });
+
+  if (!category) {
+    throw new NotFoundException(`Category with ID ${categoryId} not found`);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} menuitem`;
+
+  const allItems = await this.menuItemRepository.find({
+    where: { category: { id: categoryId } },
+
+  });
+
+  // 3. Handle empty items
+  if (allItems.length === 0) {
+    return {
+      message: `No menu items found for category ${category.categoryName}`,
+      items: [],
+    };
   }
 
-  update(id: number, updateMenuitemDto: UpdateMenuitemDto) {
-    return `This action updates a #${id} menuitem`;
+
+  return {
+
+    items: allItems,
+  };
+}
+
+
+  
+
+  updateMenuItem(id: number, updateMenuitemDto: UpdateMenuitemDto) {
+    
+
+
+ 
   }
 
-  remove(id: number) {
+  removeMenuItem(id: number) {
     return `This action removes a #${id} menuitem`;
   }
 }
