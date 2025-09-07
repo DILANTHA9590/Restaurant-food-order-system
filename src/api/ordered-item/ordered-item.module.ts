1a08eb9adc360788075364c3bc588d257ded9d1d
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-09-07 22:31:34 +0530

    🗂️ Add TypeORM entity to OrderedItemModule for DB integration 🔗

--- src/api/ordered-item/ordered-item.module.ts
+++ src/api/ordered-item/ordered-item.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { OrderedItemService } from './ordered-item.service';
 import { OrderedItemController } from './ordered-item.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { OrderedItem } from './entities/ordered-item.entity';
 
 @Module({
+    imports: [TypeOrmModule.forFeature([OrderedItem])],
   controllers: [OrderedItemController],
   providers: [OrderedItemService],
 })
