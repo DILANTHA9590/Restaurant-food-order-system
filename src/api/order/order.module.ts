17c6f32b5b2bac45948f380419dd1c61d5733475
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-09-07 22:30:28 +0530

    🗂️ Add TypeORM entities to OrderModule for DB integration 🔗

--- src/api/order/order.module.ts
+++ src/api/order/order.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Order } from './entities/order.entity';
+import { Menuitem } from '../menuitem/entities/menuitem.entity';
+import { OrderedItem } from '../ordered-item/entities/ordered-item.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Order , Menuitem , OrderedItem])],
   controllers: [OrderController],
   providers: [OrderService],
 })
