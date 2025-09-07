ff1601ebf5d9a0acb979cbb100e117f3130aac0f
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-09-07 22:28:44 +0530

    🗂️ Add TypeORM entities to MenuitemModule for DB integration 🔗

--- src/api/menuitem/menuitem.module.ts
+++ src/api/menuitem/menuitem.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MenuitemService } from './menuitem.service';
 import { MenuitemController } from './menuitem.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Menuitem } from './entities/menuitem.entity';
+import { Category } from '../category/entities/category.entity';
+import { OrderedItem } from '../ordered-item/entities/ordered-item.entity';
 
 @Module({
+    imports: [TypeOrmModule.forFeature([Menuitem ,Category, OrderedItem])],
   controllers: [MenuitemController],
   providers: [MenuitemService],
 })
