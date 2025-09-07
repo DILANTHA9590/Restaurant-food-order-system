db04b303d351e21bc9846af1357b34f436d411e7
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-09-07 22:31:57 +0530

    ✨ Add validation rules to CreateOrderedItemDto 📝

--- src/api/ordered-item/dto/create-ordered-item.dto.ts
+++ src/api/ordered-item/dto/create-ordered-item.dto.ts
@@ -1,1 +1,31 @@
-export class CreateOrderedItemDto {}
+import { IsString, IsUUID, IsInt, IsOptional, IsNumber, IsNotEmpty, IsArray } from "class-validator";
+
+export class CreateOrderedItemDto {
+  @IsOptional()
+  @IsString({ message: "ProductName must be a valid string" })
+  ProductName?: string;
+
+  @IsNotEmpty()
+  id:string
+
+  @IsNotEmpty()
+  @IsInt({ message: "Quantity must be an integer number" })
+  quantity?: number;
+
+  @IsOptional()
+  @IsNumber({}, { message: "Last price must be a valid number" })
+  lastprice?: number;
+
+  @IsArray({ message: "Images must be an array" })
+  @IsOptional()
+  @IsNotEmpty({ message: "Images array cannot be empty" })
+  image?: string[];
+
+  @IsOptional()
+  @IsUUID("4", { message: "OrderId must be a valid UUID (v4)" })
+  orderId?: string;
+
+  @IsOptional()
+  @IsUUID("4", { message: "MenuItemId must be a valid UUID (v4)" })
+  menuItemId?: string;
+}
