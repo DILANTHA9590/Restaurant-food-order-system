4f4d3e6d827007896ac43108a3273cda8d5b32da
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-09-19 22:31:07 +0530

    create  tableentity with  booking one to many realtion🌟✅

---
+++ src/api/table/entities/table.entity.ts
@@ -0,0 +1,34 @@
+import { Booking } from "src/api/booking/entities/booking.entity";
+import { Column, Entity, PrimaryGeneratedColumn, OneToMany } from "typeorm";
+
+export enum RoomStatus {
+  BOOKED = "Booked",
+  AVAILABLE = "Available",
+}
+
+@Entity('tables')
+export class Table {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column({ nullable: false, unique: true })
+  tableId: string;
+
+  @Column({
+    nullable: false,
+    type: "enum",
+    enum: RoomStatus,
+    default: RoomStatus.AVAILABLE,
+  })
+  status: RoomStatus;
+
+  @Column("simple-json", { nullable: false })
+  image: string[];
+
+  @Column("text", { nullable: true })
+  description: string;
+
+  // Relations
+  @OneToMany(() => Booking, (booking) => booking.table)
+  bookings: Booking[];
+}
