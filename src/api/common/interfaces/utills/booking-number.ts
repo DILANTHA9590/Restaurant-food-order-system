c085bf1b4d370cf2342b8797fba38285b0536434
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-09-21 22:15:08 +0530

    implement generateId utility for booking and table IDs

---
+++ src/api/common/interfaces/utills/booking-number.ts
@@ -0,0 +1,12 @@
+export function genarateId(prefix:string,count:number){
+
+    if(prefix != "TBL"){
+     const year = new Date().getFullYear().toString()
+     return  `${prefix}-${year}-${(Math.floor(100000 + Math.random() * 900000).toString() + count.toString())}`
+
+   }
+
+   return `${prefix}-${(1155*2)+count}`
+   
+
+}
