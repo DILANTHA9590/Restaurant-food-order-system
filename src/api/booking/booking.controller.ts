7e846fdba065ffecdf4789504e383787af2fac6c
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-09-25 23:20:58 +0530

    create create booking controller

--- src/api/booking/booking.controller.ts
+++ src/api/booking/booking.controller.ts
@@ -1,15 +1,20 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Req, UseGuards } from '@nestjs/common';
 import { BookingService } from './booking.service';
 import { CreateBookingDto } from './dto/create-booking.dto';
 import { UpdateBookingDto } from './dto/update-booking.dto';
-
+import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
+import { JwtPayloadDto } from '../common/interfaces/jwt-payload.dto';
+import { RolesGuard } from '../auth/role-guard/roles.guard';
+@UseGuards(JwtAuthGuard ,RolesGuard)
 @Controller('booking')
 export class BookingController {
   constructor(private readonly bookingService: BookingService) {}
 
-  @Post()
-  create(@Body() createBookingDto: CreateBookingDto) {
-    return this.bookingService.create(createBookingDto);
+  @Post(':id')
+  create(@Param('id') id:string, @Req()req:any , @Body() createBookingDto: CreateBookingDto) {
+  
+  console.log(id)  // console.log(req.user.sub);
+    return this.bookingService.create( id, req, createBookingDto);
   }
 
 
