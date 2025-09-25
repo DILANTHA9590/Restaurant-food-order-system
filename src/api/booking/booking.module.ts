c0910764096bb08361c9b69a9e2c09dbdef6bf02
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-09-25 23:21:57 +0530

    add table entitiy booking module .ts🌟

--- src/api/booking/booking.module.ts
+++ src/api/booking/booking.module.ts
@@ -3,9 +3,10 @@ import { BookingService } from './booking.service';
 import { BookingController } from './booking.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Booking } from './entities/booking.entity';
+import { Table } from '../table/entities/table.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Booking])],
+    imports: [TypeOrmModule.forFeature([Booking,Table])],
   controllers: [BookingController],
   providers: [BookingService],
 })
