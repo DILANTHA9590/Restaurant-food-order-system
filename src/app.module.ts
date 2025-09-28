1fda017c972a2958d8233c4bfe4db8e6e00b88e9
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-09-28 22:56:33 +0530

    add cloud mysql db link

--- src/app.module.ts
+++ src/app.module.ts
@@ -28,16 +28,15 @@ import { Table } from './api/table/entities/table.entity';
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
         type: 'mysql',
-        url: configService.get<string>('DATABASE_URL'),
-        // host: configService.get<string>('DB_HOST'),
-        // port: configService.get<number>('DB_PORT'),
-        // username: configService.get<string>('DB_USERNAME'),y
-        // password: configService.get<string>('DB_PASS'),
-        // database: configService.get<string>('DB_NAME'),
-        // entities: [User,Category,Menuitem,Order ,OrderedItem,Booking,Table],
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASS'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [User,Category,Menuitem,Order ,OrderedItem,Booking,Table],
         autoLoadEntities: true,
-        synchronize: true,
-        // logging: true,
+        synchronize: false,
+        logging: true,
       }),
       inject: [ConfigService],
     }),
@@ -60,3 +59,72 @@ import { Table } from './api/table/entities/table.entity';
   ],
 })
 export class AppModule {}
+
+
+
+
+// import { Module } from '@nestjs/common';
+
+// import { UserModule } from './api/user/user.module';
+// import { ConfigModule, ConfigService } from '@nestjs/config';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { User } from './api/user/entities/user.entity';
+// import { AuthModule } from './api/auth/auth.module';
+// import { CategoryModule } from './api/category/category.module';
+// import { MenuitemModule } from './api/menuitem/menuitem.module';
+// import { OrderModule } from './api/order/order.module';
+// import { OrderedItemModule } from './api/ordered-item/ordered-item.module';
+// import { Category } from './api/category/entities/category.entity';
+// import { Menuitem } from './api/menuitem/entities/menuitem.entity';
+// import { Order } from './api/order/entities/order.entity';
+// import { OrderedItem } from './api/ordered-item/entities/ordered-item.entity';
+// import { BookingModule } from './api/booking/booking.module';
+// import { Booking } from './api/booking/entities/booking.entity';
+// import { TableModule } from './api/table/table.module';
+// import { Table } from './api/table/entities/table.entity';
+
+// @Module({
+//   imports: [
+//     ConfigModule.forRoot({
+//       isGlobal: true,
+//       envFilePath: '.env',
+//     }),
+//     TypeOrmModule.forRootAsync({
+//       imports: [ConfigModule],
+//       useFactory: async (configService: ConfigService) => ({
+//         type: 'mysql',
+//         url: configService.get<string>('DATABASE_URL'),
+//         // host: configService.get<string>('DB_HOST'),
+//         // port: configService.get<number>('DB_PORT'),
+//         // username: configService.get<string>('DB_USERNAME'),
+//         // password: configService.get<string>('DB_PASS'),
+//         // database: configService.get<string>('DB_NAME'),
+//         // entities: [User,Category,Menuitem,Order ,OrderedItem,Booking,Table],
+//         autoLoadEntities: true,
+//         synchronize: true,
+//         // logging: true,
+//       }),
+//       inject: [ConfigService],
+//     }),
+//     // TypeOrmModule.forFeature([User,Category,Menuitem,Order ,OrderedItem,Booking,Table]),
+
+//     UserModule,
+
+//     AuthModule,
+
+//     CategoryModule,
+
+//     MenuitemModule,
+
+//     OrderModule,
+
+//     OrderedItemModule,
+
+//     BookingModule,
+//     TableModule,
+//   ],
+// })
+// export class AppModule {}
+
+
+        
