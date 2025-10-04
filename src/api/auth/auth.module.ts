a9564c015f43d816f530eca3cc1a6a35ae873e3e
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-10-04 23:02:46 +0530

    add UserVerification entity to TypeORM module

--- src/api/auth/auth.module.ts
+++ src/api/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { User } from '../user/entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserVerification } from '../user-verification/entities/user-verification.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User,UserVerification])],
   controllers: [AuthController],
   providers: [AuthService],
 })
