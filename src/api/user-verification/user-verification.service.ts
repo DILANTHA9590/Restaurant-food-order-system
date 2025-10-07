dd3b8d887e7a90036194fa05990be7d47e94ba70
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-10-07 23:11:41 +0530

    ✅ Implement user OTP verification logic with validation and expiry checks

--- src/api/user-verification/user-verification.service.ts
+++ src/api/user-verification/user-verification.service.ts
@@ -1,26 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserVerificationDto } from './dto/create-user-verification.dto';
 import { UpdateUserVerificationDto } from './dto/update-user-verification.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserVerification } from './entities/user-verification.entity';
+import { Repository } from 'typeorm';
+import { User } from '../user/entities/user.entity';
 
 @Injectable()
 export class UserVerificationService {
-  create(createUserVerificationDto: CreateUserVerificationDto) {
-    return 'This action adds a new userVerification';
+ constructor(
+    @InjectRepository(UserVerification)
+    private readonly userVerifyRepository:Repository<UserVerification>,
+       @InjectRepository(User)
+        private readonly userRepository: Repository<User>,
+  ) {}
+
+async verifyOtp( dto: UpdateUserVerificationDto) {
+
+
+  const existing_email = await this.userVerifyRepository.find({
+  where: { email: dto.email },
+  order: { createdAt: 'DESC' },
+});
+
+if(existing_email.length == 0){
+   throw new BadRequestException('No OTP found for this email. Please request a new one.');
+}
+const [verifyRecord]= existing_email
+
+if(verifyRecord.otp !=  dto.otp){
+
+throw new BadRequestException('Invalid OTP. Please check and try again.');
+
+}
+  const now = new Date();
+  if (verifyRecord.expireTime && verifyRecord.expireTime < now) {
+    throw new BadRequestException('OTP has expired. Please request a new one.');
   }
 
-  findAll() {
-    return `This action returns all userVerification`;
-  }
+await this.userRepository.update({email:dto.email} , {isVerified:true})
 
-  findOne(id: number) {
-    return `This action returns a #${id} userVerification`;
-  }
 
-  update(id: number, updateUserVerificationDto: UpdateUserVerificationDto) {
-    return `This action updates a #${id} userVerification`;
-  }
+return{
+  statusCode:HttpStatus.OK,
+  message :"User verification  successfullly"
+}
 
-  remove(id: number) {
-    return `This action removes a #${id} userVerification`;
-  }
+
+  
+
+  
+}
+
 }
