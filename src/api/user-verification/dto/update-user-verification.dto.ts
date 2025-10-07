e9ca6a23b4fb9ceb4832e698e185f22823e83d0c
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-10-07 23:15:46 +0530

    🛠️ Update UpdateUserVerificationDto with new fields and remove inheritance from CreateUserVerificationDto

--- src/api/user-verification/dto/update-user-verification.dto.ts
+++ src/api/user-verification/dto/update-user-verification.dto.ts
@@ -1,4 +1,14 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateUserVerificationDto } from './create-user-verification.dto';
+import { IsEmail, IsNumber } from 'class-validator';
 
-export class UpdateUserVerificationDto extends PartialType(CreateUserVerificationDto) {}
+export class UpdateUserVerificationDto  {
+
+      @ApiProperty({ example: 'dilantha9590@gmail.com' })
+      @IsEmail({}, { message: 'Please enter a valid email address.' })
+      email: string;
+
+      @ApiProperty({ example: '482913' })
+      @IsNumber({},{ message: 'OTP must be a number.' })
+      otp: Number;
+}
