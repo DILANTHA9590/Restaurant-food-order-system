3a9edb5c793c88a70c6177bf674bb65f38de3f83
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-10-07 22:39:50 +0530

    🔗 Import MailModule into AuthModule for email verification support

--- src/api/auth/auth.module.ts
+++ src/api/auth/auth.module.ts
@@ -4,9 +4,12 @@ import { AuthController } from './auth.controller';
 import { User } from '../user/entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserVerification } from '../user-verification/entities/user-verification.entity';
+import { MailModule } from '../mail/mail.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User,UserVerification])],
+  imports: [TypeOrmModule.forFeature([User,UserVerification]),
+   MailModule
+],
   controllers: [AuthController],
   providers: [AuthService],
 })
