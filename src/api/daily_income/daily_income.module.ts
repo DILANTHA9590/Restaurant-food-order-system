c1f1d8afa0f4db30cf3e70ef68d1ee5deaa839db
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-10-14 23:26:06 +0530

    📦 add Order entity to DailyIncomeModule for revenue calculation

--- src/api/daily_income/daily_income.module.ts
+++ src/api/daily_income/daily_income.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { DailyIncome } from './entities/daily_income.entity';
 import { DailyIncomeService } from './daily_income.service';
 import { DailyIncomeController } from './daily_income.controller';
+import { Order } from '../order/entities/order.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([DailyIncome])],
+  imports: [TypeOrmModule.forFeature([DailyIncome,Order])],
   controllers: [DailyIncomeController],
   providers: [DailyIncomeService],
   exports: [DailyIncomeService],
