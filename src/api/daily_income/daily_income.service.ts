75463329664fdf1aaae083bee8b1b8305b35fc94
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-10-14 23:24:25 +0530

    create ddaily income creation sevice method🌟

--- src/api/daily_income/daily_income.service.ts
+++ src/api/daily_income/daily_income.service.ts
@@ -1,8 +1,9 @@
-import { Injectable, BadRequestException, NotFoundException } from '@nestjs/common';
+import { Injectable, BadRequestException, NotFoundException, HttpStatus } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Between, Repository } from 'typeorm';
 import { DailyIncome } from './entities/daily_income.entity';
-import { CreateDailyIncomeDto } from './dto/create-daily-income.dto';
+import { Order } from '../order/entities/order.entity';
+import { CreateOrderDto } from '../order/dto/create-order.dto';
 import { UpdateDailyIncomeDto } from './dto/update-daily-income.dto';
 
 @Injectable()
@@ -10,17 +11,41 @@ export class DailyIncomeService {
   constructor(
     @InjectRepository(DailyIncome)
     private readonly dailyIncomeRepository: Repository<DailyIncome>,
+     @InjectRepository(Order)
+    private readonly orderRepository:Repository<Order>,
   ) {}
 
-  async create(createDailyIncomeDto: CreateDailyIncomeDto) {
-    const existing = await this.dailyIncomeRepository.findOne({
-      where: { date: createDailyIncomeDto.date },
-    });
-    if (existing) {
-      throw new BadRequestException('Daily income for this date already exists');
-    }
-    const dailyIncome = this.dailyIncomeRepository.create(createDailyIncomeDto);
-    return this.dailyIncomeRepository.save(dailyIncome);
+  async create() {
+    const today = new Date();
+    const startTime = new Date(); 
+    startTime.setHours(0, 0, 0, 0);
+    const endTime = new Date(); 
+    endTime.setHours(23, 59, 59, 999);
+    
+const orders: CreateOrderDto[]= await this.orderRepository.find({where:{createdAt:Between(startTime,endTime)}})
+if(orders.length==0){
+  return console.log("No today Orders Found")
+}
+
+let totalIncome:number =0
+for(const order of orders){
+  const {totalPrice} =order
+  if(totalPrice){
+    totalIncome += totalPrice
+  }
+
+}
+
+const saveddailyIncome= this.dailyIncomeRepository.create({
+    date: today.toISOString().split('T')[0],
+    totalIncome:totalIncome,
+    totalOrders:orders.length,    
+  })
+
+  await this.dailyIncomeRepository.save(saveddailyIncome)
+
+  return console.log("daily income saved succesfully")
+
   }
 
   async findAll() {
