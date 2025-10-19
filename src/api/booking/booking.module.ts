a46360fe4c83ab72c0dec36f8aafe9ddb7761fd4
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-10-19 21:17:46 +0530

    add mail module✅

--- src/api/booking/booking.module.ts
+++ src/api/booking/booking.module.ts
@@ -4,9 +4,11 @@ import { BookingController } from './booking.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Booking } from './entities/booking.entity';
 import { Table } from '../table/entities/table.entity';
+import { MailModule } from '../mail/mail.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Booking, Table])],
+  imports: [TypeOrmModule.forFeature([Booking, Table]),
+MailModule],
   controllers: [BookingController],
   providers: [BookingService],
 })
