06f1a5a322a295b87458485f4e622e724b1775d7
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-10-19 21:14:59 +0530

    import mail module❇️

--- src/api/table/table.module.ts
+++ src/api/table/table.module.ts
@@ -3,10 +3,13 @@ import { TableService } from './table.service';
 import { TableController } from './table.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Table } from './entities/table.entity';
+import { MailModule } from '../mail/mail.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Table])],
+  imports: [TypeOrmModule.forFeature([Table]),
+   MailModule],
   controllers: [TableController],
   providers: [TableService],
 })
 export class TableModule {}
+
